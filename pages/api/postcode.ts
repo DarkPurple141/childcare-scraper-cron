3a08b49b7c6cebc0ab97eb62708988cbc84a2cb4
Alex Hinds <alex.hinds141@gmail.com>
2021-10-08 13:03:35 +1100

    chore: test

---
+++ pages/api/postcode.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { runPostcode } from '../../src/run-postcode'
+import { simplifyLocality } from '../../src/utils'
+import { launch } from 'puppeteer'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      const { authorization } = req.headers
+
+      if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
+        const browser = await launch({ headless: true })
+        const page = await browser.newPage()
+        const data = await runPostcode(
+          page,
+          simplifyLocality({
+            id: 4497,
+            postcode: '2010',
+            locality: 'DARLINGHURST',
+            state: 'NSW',
+            long: 151.212262,
+            lat: -33.884119,
+            dc: 'WATERLOO DELIVERY FACILITY',
+            type: 'Delivery Area',
+            status: 'Updated 6-Feb-2020',
+            sa3: '11703',
+            sa3name: 'Sydney Inner City',
+            sa4: '117',
+            sa4name: 'Sydney - City and Inner South',
+            region: 'R1',
+          })
+        )
+        res.status(200).json(data)
+      } else {
+        res.status(401).json({ success: false })
+      }
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err.message })
+    }
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+}
